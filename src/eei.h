GreatAttractor/hera

EEI: charge for memory gas cost in useGas

src/eei.h
@@ -113,6 +113,9 @@ private:
   struct evm_message const& msg;
   std::vector<uint8_t> lastReturnData;
   ExecutionResult & result;
+
+  static constexpr unsigned memory_page_size = 65536;
+  static constexpr unsigned memory_cost = 1;
 };
 
 struct GasSchedule {

WinBuilds/ethvm-hera

src/eei.h
@@ -99,6 +99,9 @@ private:
 };
 
 struct GasSchedule {
+  static constexpr unsigned memoryPageSize = 65536;
+  static constexpr unsigned memoryCostPerPage = 1;
+
   static constexpr unsigned storageLoad = 200;
   static constexpr unsigned storageStoreCreate = 20000;
   static constexpr unsigned storageStoreChange = 5000;