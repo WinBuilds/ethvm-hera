8868caf6098281578320807fa1955c0f095c69d7
Paweł Bylica <chfast@gmail.com>
2018-08-03 00:57:16 +0200

    ProjectWabt: Final tweaks

--- cmake/ProjectWabt.cmake
+++ cmake/ProjectWabt.cmake
@@ -5,13 +5,11 @@ set(ProjectWabtIncluded TRUE)
 
 include(ExternalProject)
 
-
-
 set(prefix ${CMAKE_BINARY_DIR}/deps)
 set(source_dir ${prefix}/src/wabt)
 set(binary_dir ${prefix}/src/wabt-build)
 set(include_dir ${source_dir})
-set(wabt_library ${prefix}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}wabt{CMAKE_STATIC_LIBRARY_SUFFIX})
+set(wabt_library ${binary_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}wabt${CMAKE_STATIC_LIBRARY_SUFFIX})
 
 ExternalProject_Add(wabt
     PREFIX ${prefix}
@@ -25,7 +23,7 @@ ExternalProject_Add(wabt
     -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
     -DCMAKE_BUILD_TYPE=Release
     -DBUILD_TESTS=OFF
-    -DCMAKE_INSTALL_LIBDIR=lib
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
     BUILD_BYPRODUCTS ${wabt_library}
 )
 
